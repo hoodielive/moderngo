4a20a59b4629f0e7e463ab12d267009527f0a7ca
hoodielive <hoodielive@gmail.com>
2022-06-05 00:10:22 -0400

    net/http

--- 2022/lenslocked/main.go
+++ 2022/lenslocked/main.go
@@ -1,6 +1,19 @@
 package main
 
-import (
-	format
+import 
+(
+	"fmt"
+	"net/http"
 )
 
+func handlerFunc(w http.Response, r *http.Request)
+{
+	fmt.Fprint(w, "<h1>Welcome to the Egun Awo Site.")
+}
+
+func main()
+{
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", nil)
+}
